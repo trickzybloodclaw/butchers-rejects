35106fc3630c84d83dd4e9ec5374a31cbd2e6e31
trickzybloodclaw <keyven69@gmail.com>
2026-09-19 19:20:17 -0300

    Update data.js

--- data.js
+++ data.js
@@ -15,3 +15,60 @@ window.GUILD_DATA = {
     {title:'SPOILS OF WAR',image:'',caption:'Show off your greatest victories.'}
   ]
 };
+
+window.GUILD_DATA = {
+  roster: [
+    {
+      name: 'Bigbull Ironhoof',
+      role: 'GUILD MASTER',
+      className: 'Guardian Druid',
+      initial: 'GM',
+      image: 'images/guild-master.png'
+    },
+    {
+      name: 'Your Raid Leader',
+      role: 'RAID LEADER',
+      className: 'Add character name & class',
+      initial: 'RL',
+      image: 'images/raid-leader.png'
+    },
+    {
+      name: 'Your Warband',
+      role: 'GUILD MEMBERS',
+      className: 'Your warriors belong here',
+      initial: '⚔',
+      image: 'images/guild-members.png'
+    }
+  ],
+
+  news: [
+    {
+      date: 'GUILD ANNOUNCEMENT',
+      title: 'The warband is recruiting',
+      body: 'Join us for dungeons, raids, questing, and endless carnage. Enter our Discord to meet the guild.'
+    },
+    {
+      date: 'COMING SOON',
+      title: 'The next battle awaits',
+      body: 'Guild officers can post upcoming events and raid announcements by editing data.js.'
+    }
+  ],
+
+  gallery: [
+    {
+      title: 'THE FIRST VICTORY',
+      image: '',
+      caption: 'Your first guild screenshot goes here.'
+    },
+    {
+      title: 'THE WAR CAMP',
+      image: '',
+      caption: 'Capture your warband in action.'
+    },
+    {
+      title: 'SPOILS OF WAR',
+      image: '',
+      caption: 'Show off your greatest victories.'
+    }
+  ]
+};
